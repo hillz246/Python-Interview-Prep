092973cf664c8be8b657a65e57be74cb31ecb626
Prachi Patel <hillz246@gmail.com>
2025-06-03 17:56:41 -0400

    linked_list

--- section4_linkedlist/append.py
+++ section4_linkedlist/append.py
@@ -1,10 +1,10 @@
-## LL: Print List
-# Implement a method print_list(self) that prints the linked list's elements, one per line.
+# Append
 
 class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+
 
 
 class LinkedList:
@@ -14,30 +14,50 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
-    def make_empty(self):
-        self.head = None
-        self.tail = None
-        self.length = 0
 
-    def append(self, value):
-        new_node = Node(value)
-        if self.head is None:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.next = new_node
-            self.tail = new_node
-        self.length += 1
+    def linked_list(self,value):
+        temp = self.head
+        while self is not None:
+            print(temp.value)
+        temp = temp.next
+
 
     def print_list(self):
         temp = self.head
         while temp is not None:
             print(temp.value)
             temp = temp.next
-    
+
+
+    def make_empty(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:   ## to see is list is empty
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+        self.length += 1
+        return True
 
 my_linked_list = LinkedList(1)
-my_linked_list.append(2)
-my_linked_list.append(3)
+my_linked_list.make_empty()
 
+my_linked_list.append(1)
+my_linked_list.append(2)
+
+print('Head:', my_linked_list.head.value)
+print('Tail:', my_linked_list.tail.value)
+print('Length:', my_linked_list.length, '\n')
+
+print('Linked List:')
 my_linked_list.print_list()
+
+        
+    
